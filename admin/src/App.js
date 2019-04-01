4475cc886d650e9050976431e1c669223409a1e1
Yamna Melki <melkiyamna@protonmail.com>
2019-04-01 16:35:01 +0200

    essai mise en place barre de recherche admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,24 @@
 import React from 'react';
 import { HydraAdmin } from '@api-platform/admin';
+import { Filter, ReferenceInput, SelectInput, TextInput, List } from 'react-admin';
+import parseHydraDocumentation from '@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation';
 
-export default () => <HydraAdmin entrypoint="http://localhost:8000/api"/>;
+const entrypoint = 'http://localhost:8000/api';
+
+const myApiDocumentationParser = entrypoint => parseHydraDocumentation(entrypoint);
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="Child" source="childId" reference="children" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+export const PostList = (props) => (
+    <List {...props} filters={<PostFilter />}>
+    </List>
+);
+
+
+export default (props) => <HydraAdmin apiDocumentationParser={myApiDocumentationParser} entrypoint={entrypoint}/>;
