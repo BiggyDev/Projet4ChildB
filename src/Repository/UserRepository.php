1789c5f4b162e2d23abe26cf012fb3365c513729
BiggyDev <vincent.carpentier111@gmail.com>
2019-04-03 14:38:04 +0200

    Modif JWT - A revoir

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -13,19 +12,11 @@ use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
+class UserRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, User::class);
     }
 
-    public function loadUserByUsername($usernameOrEmail)
-    {
-        return $this->createQueryBuilder('u')
-            ->where('u.username = :query OR u.email = :query')
-            ->setParameter('query', $usernameOrEmail)
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
 }
