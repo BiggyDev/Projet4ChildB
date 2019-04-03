3bb753d77eaab2c8c66b01ed87a263884a389b9a
Druxys <drudrux@gmail.com>
2019-04-03 16:45:12 +0200

    création profil controller

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function index()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function showProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function showProfilProvider()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function modifyProfilClient()
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+
+}
