01d6c06ddd4175e99b5fee55337fcebcaff83e01
Nicolas Becuwe <niconzeweb@wanadoo.fr>
2019-04-03 14:59:02 +0200

    Controllers creation

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicon
+ * Date: 03/04/2019
+ * Time: 14:53
+ */
+
+namespace App\Controller;
+
+
+class NotificationController
+{
+
+}
---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicon
+ * Date: 03/04/2019
+ * Time: 14:54
+ */
+
+namespace App\Controller;
+
+
+class ReservationController
+{
+
+}
