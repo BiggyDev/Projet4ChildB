b6b28decd085fe7b762f295c96d559965beb2490
Druxys <drudrux@gmail.com>
2019-04-05 14:43:13 +0200

    méthode showProfilClient

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -28,32 +28,28 @@ class ProfilController extends AbstractController
      */
     public function showProfilClient($id)
     {
-        $profilClient = new Client();
           $profil = $this->getDoctrine()
             ->getRepository(Client::class)
             ->find($id);
-
-//        $profil = $profilClientRepository->getInfoProfilClient($id);
 
         if (!$profil) {
             throw $this->createNotFoundException(
                 'Aucun profil trouvé pour cet id : '.$id
             );
         }
+        $profil = $this->get('serializer')->serialize($profil, 'json');
 
-        $response = new JsonResponse($profil);
-        $response = new Response(json_encode($profil));
+        $response = new Response($profil);
         $response->headers->set('Content-Type', 'application/json');
-//        $response->setData([
-//            'name' => $profil
+
+        return $response;
+
+        // DEBUG : VUE TEST \\
+//        return $this->render('profil/index.html.twig', [
+//            'name' => dump($response),
 //        ]);
-
-        return $this->render('profil/index.html.twig', [
-            'name' => dump($profil),
-        ]);
-//        return dump($response);
-
     }
+
 //
 //    /**
 //     * @Route("/profil", name="profil")
