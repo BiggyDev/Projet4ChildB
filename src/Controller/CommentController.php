3cbe0579b977c9734414c735d9b21007fffd9c97
Nicolas Becuwe <niconzeweb@wanadoo.fr>
2019-04-19 15:09:00 +0200

    requetes comment (sans routes)

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,29 +2,149 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
 
-    public function postCommentProvider($idClient, $idProvider)
+    public function postCommentProvider($id, $idClient, $idProvider, $title, $article, $score)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
 
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No client found for id '.$id
+            );
+        }
+
+        $comment = new Comment();
+        $comment->setClient($idClient);
+        $comment->setProvider($idProvider);
+        $comment->setTitle($title);
+        $comment->setArticle($article);
+        $comment->setScore($score);
+
+        $entityManager->flush();
+
+        return new Response('Comment sent'. $comment->getId());
     }
 
-    public function showCommentProvider($idClient, $idProvider)
+    public function modifyCommentProvider($id, $idClient, $idProvider, $title, $article, $score)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
 
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No comment found'
+            );
+        }
+        $comment = new Comment();
+        $comment->setClient($idClient);
+        $comment->setProvider($idProvider);
+        $comment->setTitle($title);
+        $comment->setArticle($article);
+        $comment->setScore($score);
+
+        $entityManager->flush();
+
+        return new Response('Comment edited'. $comment->getId());
     }
 
-    public function sendCommentClient($idClient, $idProvider)
+    public function showCommentProvider($id, $idProvider)
     {
+        $comment = $this_>getDoctrine()
+            ->getRepository(Comment::class)
+            ->find($id, $idProvider);
 
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'Aucun commentaire trouvé'
+            );}
+        $comment = $this->get('serializer')->serialize($profil, 'json');
+
+        $response = new Response($comment);
+        $response->headers->set('Content-Type', 'applicaton/json')
     }
 
-    public function showCommentClient($idClient, $idProvider)
+    public function postCommentClient($id, $idClient, $idProvider, $title, $article, $score)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
 
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No comment found'
+            );
+        }
+
+        $comment = new Comment();
+        $comment->setClient($idClient);
+        $comment->setProvider($idProvider);
+        $comment->setTitle($title);
+        $comment->setArticle($article);
+        $comment->setScore($score);
+
+        $entityManager->flush();
+
+        return new Response('Comment sent'. $comment->getId());
+    }
+
+    public function modifyCommentClient($id, $idClient, $idProvider, $title, $article, $score)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No comment found'
+            );
+        }
+        $comment = new Comment();
+        $comment->setClient($idClient);
+        $comment->setProvider($idProvider);
+        $comment->setTitle($title);
+        $comment->setArticle($article);
+        $comment->setScore($score);
+
+        $entityManager->flush();
+        return new Response('Comment edited'. $comment->getId());
+    }
+
+    public function showCommentClient($id, $idClient)
+    {
+        $comment = $this_>getDoctrine()
+                ->getRepository(Comment::class)
+                ->find($id, $idClient);
+
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'Aucun commentaire trouvé'
+            );}
+
+        $comment = $this->get('serializer')->serialize($profil, 'json');
+        $response = new Response($comment);
+        $response->headers->set('Content-Type', 'applicaton/json')
+    }
+
+    /**
+     * @Route("comments/delete/{id}", name="comment_delete")
+     */
+    public function deleteComment($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException(
+                'No comment found '
+            );
+        }
+
+        $entityManager->remove($comment);
+        $entityManager->flush();
     }
 }
