fc161e9006816aa30bede8ee432f4814043267ef
TURPIN <drudrux@gmail.com>
2019-04-23 10:01:14 +0200

    debug commentcontroller

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class CommentController extends AbstractController
 {
