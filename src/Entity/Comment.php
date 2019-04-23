482ae033cf6f4743a4f4f44ba6eda8f2605bf0dc
mathD92 <math.delp76@gmail.com>
2019-04-23 19:07:48 +0200

    commit du soir Admin wip

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -120,6 +122,6 @@ class Comment
 
     public function __toString()
     {
-        return $this->name;
+        return $this->title;
     }
 }
